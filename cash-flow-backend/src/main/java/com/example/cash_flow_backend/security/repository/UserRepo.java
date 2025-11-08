bcf546bf51fd32d7379833bd9b996ea62a1ecd8a
davidStupen <davidstupen4@seznam.cz>
2025-11-08 17:24:37 +0100

    smazání      Optional<User> findByEmail(String email);     Optional<User> findByProfileImg(String profileImg); v UserRepo. backend

--- cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/repository/UserRepo.java
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/repository/UserRepo.java
@@ -9,6 +9,4 @@ import java.util.Optional;
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
-    Optional<User> findByEmail(String email);
-    Optional<User> findByProfileImg(String profileImg);
 }
