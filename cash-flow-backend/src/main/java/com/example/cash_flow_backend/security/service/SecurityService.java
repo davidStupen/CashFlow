5bf290ac469f1b64d0a26e22ba2048eab515555c
davidStupen <davidstupen4@seznam.cz>
2025-11-08 13:01:02 +0100

    uložení user do DB backend

--- cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/service/SecurityService.java
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/service/SecurityService.java
@@ -4,11 +4,19 @@ import com.example.cash_flow_backend.security.UserException;
 import com.example.cash_flow_backend.security.model.Role;
 import com.example.cash_flow_backend.security.model.User;
 import com.example.cash_flow_backend.security.repository.UserRepo;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -39,11 +47,19 @@ public class SecurityService {
         }
         return false;
     }
-    public ResponseEntity<?> saveUser(User user) throws UserException {
-        if (this.isExistUserInDB(user)){
+    public ResponseEntity<?> saveUser(User user, MultipartFile img) throws UserException, IOException {
+        if ( ! this.isExistUserInDB(user)){
             user.setPassword(this.encoder.encode(user.getPassword()));
             user.setRole(Role.ROLE_USER);
+            String dirPath = "data/profileImg";
+            String storageName = StringUtils.cleanPath(Objects.requireNonNull(img.getOriginalFilename()));
+            if (Files.notExists(Paths.get(dirPath))){
+                Files.createDirectories(Paths.get(dirPath));
+            }
+            Files.copy(img.getInputStream(), Paths.get(dirPath).resolve(storageName), StandardCopyOption.REPLACE_EXISTING);
+            this.userRepo.save(user);
         }
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     public ResponseEntity<?> login(User user) {
