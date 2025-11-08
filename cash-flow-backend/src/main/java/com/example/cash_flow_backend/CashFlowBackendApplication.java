379335797a08e2b39f39068cbafef1aa5a50bba7
davidStupen <davidstupen4@seznam.cz>
2025-11-08 08:59:29 +0100

    založení spring-boot

---
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/CashFlowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cash_flow_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashFlowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CashFlowBackendApplication.class, args);
+	}
+
+}
---
+++ cash-flow-backend/src/test/java/com/example/cash_flow_backend/CashFlowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cash_flow_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashFlowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
