bb753e8574f45a6fae498d74753ac0874e4485c3
davidStupen <davidstupen4@seznam.cz>
2025-11-08 12:11:26 +0100

    rozdělání saveUser backend

--- cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/service/SecurityService.java
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/service/SecurityService.java
@@ -1,8 +1,38 @@
 package com.example.cash_flow_backend.security.service;
 
+import com.example.cash_flow_backend.security.model.Role;
+import com.example.cash_flow_backend.security.model.User;
+import com.example.cash_flow_backend.security.repository.UserRepo;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class SecurityService {
-    
+    private AuthenticationManager authenticationManager;
+    private BCryptPasswordEncoder encoder;
+    private JwtService jwtService;
+    private UserRepo userRepo;
+
+    public SecurityService(AuthenticationManager authenticationManager, BCryptPasswordEncoder encoder, JwtService jwtService, UserRepo userRepo) {
+        this.authenticationManager = authenticationManager;
+        this.encoder = encoder;
+        this.jwtService = jwtService;
+        this.userRepo = userRepo;
+    }
+
+    public ResponseEntity<?> saveUser(User user) {
+        Optional<User> u = this.userRepo.findByUsername(user.getUsername());
+        if (u.isPresent()){
+
+        }
+        user.setPassword(this.encoder.encode(user.getPassword()));
+        user.setRole(Role.ROLE_USER);
+    }
+
+    public ResponseEntity<?> login(User user) {
+    }
 }
