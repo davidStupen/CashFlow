7260f4e3317e58bf8c7d4c020491ad3978555021
davidStupen <davidstupen4@seznam.cz>
2025-11-08 16:40:18 +0100

    přesměrování v registre a login frontendu

--- cash-flow-frontend/src/App.jsx
+++ cash-flow-frontend/src/App.jsx
@@ -1,11 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import Login from "./pages/Login"
+import Registry from "./pages/Registry"
+import MainPage from "./pages/MainPage"
 
 function App() {
   return(
     <div>
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/registry" element={<Registry/>}/>
+        <Route path="/main" element={<MainPage/>}/>
       </Routes>
     </div>
   )
--- cash-flow-frontend/src/pages/Login.jsx
+++ cash-flow-frontend/src/pages/Login.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react"
 import api from "../api"
+import { Link, useNavigate } from "react-router-dom"
 
 const Login = () => {
+  const navigate = useNavigate()
   const [login, setLogin] = useState({username:"", password:""})
   const [error, setError] = useState("")
   const handlerLogin = e => {
@@ -15,6 +17,7 @@ const Login = () => {
     e.preventDefault()
     try{
       await api.post("/api/auth/login", login)
+      navigate("/main")
     } catch(err){
       setError(err.response?.data)
     }
@@ -26,6 +29,7 @@ const Login = () => {
         <input type="password" name="password" value={login.password} onChange={handlerLogin}/>
         <input type="submit" value="Sign in"/>
       </form>
+      <p>Don"t have an account yet? You can sign up <Link to={"/registry"}><li>here</li></Link>.</p>
       <h3>{error}</h3>
     </div>
   )
