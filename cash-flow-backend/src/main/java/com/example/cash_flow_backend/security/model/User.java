6b3b7b0594fea86fac6fc2cd0980b588327878df
davidStupen <davidstupen4@seznam.cz>
2025-11-08 09:17:45 +0100

    založení model-user backend

---
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/model/User.java
@@ -0,0 +1,30 @@
+package com.example.cash_flow_backend.security.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false, unique = true, length = 25)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Transactionn> transactions;
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Categorie> categories;
+}
