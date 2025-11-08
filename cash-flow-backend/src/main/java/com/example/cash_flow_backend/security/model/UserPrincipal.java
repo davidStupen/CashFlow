e51e256d17071be21533fd690b6daf11fb4f89ad
davidStupen <davidstupen4@seznam.cz>
2025-11-08 09:49:55 +0100

    založení UserPrincipal backend

---
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/model/UserPrincipal.java
@@ -0,0 +1,30 @@
+package com.example.cash_flow_backend.security.model;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+public class UserPrincipal implements UserDetails {
+    private User user;
+
+    public UserPrincipal(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+}
