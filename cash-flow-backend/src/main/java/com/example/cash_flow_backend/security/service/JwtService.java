f2f0e23ae07e4d971605440d3da7e1a2aadb30b6
davidStupen <davidstupen4@seznam.cz>
2025-11-08 10:07:49 +0100

    rozdělání generateToken backend

--- cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/service/JwtService.java
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/service/JwtService.java
@@ -1,8 +1,19 @@
 package com.example.cash_flow_backend.security.service;
 
+import com.example.cash_flow_backend.security.model.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+    public String generateToken(User user){
 
+    }
+    public String extractUsername(String token) {
+
+    }
+
+    public boolean validationToken(String token, UserDetails userDetails) {
+
+    }
 }
