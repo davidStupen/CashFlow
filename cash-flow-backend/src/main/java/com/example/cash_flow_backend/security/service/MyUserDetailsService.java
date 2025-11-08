4e2c6084cde5941f810030835cd8596531a4f6f3
davidStupen <davidstupen4@seznam.cz>
2025-11-08 09:48:46 +0100

    založení MyUserDetailsService backend

--- cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/service/MyUserDetailsService.java
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/service/MyUserDetailsService.java
@@ -1,12 +1,22 @@
 package com.example.cash_flow_backend.security.service;
 
+import com.example.cash_flow_backend.security.model.User;
+import com.example.cash_flow_backend.security.repository.UserRepo;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class MyUserDetailsService implements UserDetailsService {
+    private UserRepo userRepo;
+
+    public MyUserDetailsService(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        User user = this.userRepo.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("user not find"));
+        return 
     }
 }
