92c1a33fe70e93086333aafc7336109b2df53fa8
davidStupen <davidstupen4@seznam.cz>
2025-11-08 10:10:37 +0100

    přidání UserTokenDTO backend

---
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/model/dto/UserTokenDTO.java
@@ -0,0 +1,9 @@
+package com.example.cash_flow_backend.security.model.dto;
+
+import com.example.cash_flow_backend.security.model.Role;
+
+public record UserTokenDTO(
+        String username,
+        Role role
+) {
+}
