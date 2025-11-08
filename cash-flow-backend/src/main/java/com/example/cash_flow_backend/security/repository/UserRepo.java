92a78536187656fa4ec08b48d403b47b470753da
davidStupen <davidstupen4@seznam.cz>
2025-11-08 09:43:24 +0100

    založení UserRepo backend

---
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/security/repository/UserRepo.java
@@ -0,0 +1,12 @@
+package com.example.cash_flow_backend.security.repository;
+
+import com.example.cash_flow_backend.security.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends JpaRepository<User, Integer> {
+    Optional<User> findByUsername(String username);
+}
