1ffe64ad2d091b4f38e72813e9d47652082f5187
davidStupen <davidstupen4@seznam.cz>
2025-11-09 16:59:01 +0100

    přidání do CategoryRepo metodu findByCategory

--- cash-flow-backend/src/main/java/com/example/cash_flow_backend/budgettracker/repository/CategoryRepo.java
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/budgettracker/repository/CategoryRepo.java
@@ -4,6 +4,9 @@ import com.example.cash_flow_backend.budgettracker.model.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoryRepo extends JpaRepository<Category, Integer> {
+    Optional<Category> findByCategory(String category);
 }
