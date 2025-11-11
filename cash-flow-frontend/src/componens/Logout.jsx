4656ae49f646dd308d13260e32b8d2946ef0c1f8
davidStupen <davidstupen4@seznam.cz>
2025-11-11 09:48:05 +0100

    přesměrování v Logout frontend

--- cash-flow-frontend/src/componens/Logout.jsx
+++ cash-flow-frontend/src/componens/Logout.jsx
@@ -1,6 +1,10 @@
+import { useNavigate } from "react-router-dom"
+
 const Logout = () => {
+  const navigate = useNavigate()
   const handlerClick = () => {
     localStorage.removeItem("token")
+    navigate("/")
   }
   return(
     <div>
