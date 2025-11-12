3019ebb09326997d50bedf15c01d988ffdc4b2bd
davidStupen <davidstupen4@seznam.cz>
2025-11-12 19:18:07 +0100

    ValidationEmail

--- cash-flow-backend/src/main/java/com/example/cash_flow_backend/budgettracker/service/EmailAndPDFService.java
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/budgettracker/service/EmailAndPDFService.java
@@ -1,5 +1,6 @@
 package com.example.cash_flow_backend.budgettracker.service;
 
+import com.example.cash_flow_backend.budgettracker.exception.ValidationEmailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,8 +12,32 @@ public class EmailAndPDFService {
     public EmailAndPDFService(JavaMailSender sender) {
         this.sender = sender;
     }
+    private void validationEmail(String email) throws ValidationEmailException {
+        if (email.length() < 6){
+            throw new ValidationEmailException("Email is too short " + email);
+        }
+        if ( ! email.contains("@")){
+            throw new ValidationEmailException("The email has to contain @ " + email);
+        }
+        if ( ! email.contains(".")){
+            throw new ValidationEmailException("The email has to contain dot " + email);
+        }
 
+        String[] splitEmailByDot = email.split("\\.");
+        if (splitEmailByDot[splitEmailByDot.length - 1].length() > 14){
+            throw new ValidationEmailException("Invalid email " + email + " with " + splitEmailByDot[splitEmailByDot.length - 1]);
+        }
+
+        String[] splitEmailByAt = email.split("@");
+        if (splitEmailByAt.length != 2){
+            throw new ValidationEmailException("Only one at symbol is allowed. You have " + splitEmailByAt.length);
+        }
+        if (splitEmailByAt[0].isEmpty() || splitEmailByAt[1].isEmpty()){
+            throw new ValidationEmailException("Invalid email " + email);
+        }
+    }
     public void simpleSentEmail(String email, String subject, String text) throws Exception {
+        this.validationEmail(email);
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setFrom("davidstupenarima@gmail.com");
         mailMessage.setTo(email);
