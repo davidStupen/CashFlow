b94ae9c1a5da0eed57a5e8fc040dd1fe61c6a9da
davidStupen <davidstupen4@seznam.cz>
2025-11-12 18:34:31 +0100

    ValidationEmailException

---
+++ cash-flow-backend/src/main/java/com/example/cash_flow_backend/budgettracker/exception/ValidationEmailException.java
@@ -0,0 +1,7 @@
+package com.example.cash_flow_backend.budgettracker.exception;
+
+public class ValidationEmailException extends Exception {
+    public ValidationEmailException(String message) {
+        super(message);
+    }
+}
